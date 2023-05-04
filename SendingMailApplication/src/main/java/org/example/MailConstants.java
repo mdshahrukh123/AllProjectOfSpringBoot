3ac113841ed72b7214ce242a22272db25da434a5
shahrukh <mohdshahrukhmzn1@gmail.com>
2023-05-04 17:50:27 +0530

    send Mail

---
+++ SendingMailApplication/src/main/java/org/example/MailConstants.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class MailConstants {
+    //sender
+    public static final String SENDER = "mohdshahrukhmzn1@gmail.com";
+
+   //receiver
+    public static final String RECEIVER = "shahrukhsaifi77749@gmail.com";
+
+    //subject of the mail
+    public static final String SUBJECT = "Testing Mail : 04/05/2023";
+
+    //message to be sent in the mail body
+    public static final String MESSAGE = "This mail is for security check purpose ...!!!";
+
+}
